2ae1345bfb6eefe66143887b641a9e785acad3e0
vivek <nvivek@vanenburg.com>
2026-07-03 15:55:39 +0530

    Add native GitHub Actions deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/my-portfolio/',
-  plugins: [react(),
-    tailwindcss()
+  // Ensure this matches your exact GitHub repository name
+  base: '/my-portfolio/', 
+  plugins: [
+    react(),
+    tailwindcss(),
   ],
 })
