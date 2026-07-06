0ee9ee671c82ca2b9afbe6f960aa5e0017c1ab39
vivek <nvivek@vanenburg.com>
2026-07-06 12:11:58 +0530

    FIX

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import ReactGA from 'react-ga4'
-ReactGA.initialize('G-R7HK71S74X')
-ReactGA.send({ hitType: 'pageview', page: window.location.pathname })
+// ReactGA.initialize('G-R7HK71S74X')
+// ReactGA.send({ hitType: 'pageview', page: window.location.pathname })
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
