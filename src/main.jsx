c3b61694bc2a176d496793d15f0fa9f114da2dba
vivek <nvivek@vanenburg.com>
2026-07-06 11:52:38 +0530

    Google analytics Added

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ReactGA from 'react-ga4'
+ReactGA.initialize('G-R7HK71S74X')
+ReactGA.send({ hitType: 'pageview', page: window.location.pathname })
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
